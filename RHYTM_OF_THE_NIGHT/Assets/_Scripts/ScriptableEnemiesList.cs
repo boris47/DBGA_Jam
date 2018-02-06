791f158c63414c77e61b71e5c25aa3745083a6c3
Vincenzo Villani <v.villani84@alice.it>
2018-02-06 22:42:25 +0100

    Creata sequenza spawn nemici e attivazione dei vari bottoni! Vincenzo il re

---
+++ RHYTM_OF_THE_NIGHT/Assets/_Scripts/ScriptableEnemiesList.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu( fileName = "asd", menuName = "Enemy/Create" )]
+public class ScriptableEnemiesList : ScriptableObject {
+
+    public List<EnemyVins> enemiesList;
+
+}
