7a25efaf6e66c4b9bddcbb226b33784c8c3ce900
boris47 <billgatesiswithus@gmail.com>
2018-02-06 16:02:01 +0100

    Created prefabs for beatListener and GameManager

--- RHYTM_OF_THE_NIGHT/Assets/Scripts/GameManager.cs
+++ RHYTM_OF_THE_NIGHT/Assets/Scripts/GameManager.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+	public	void	OnBeat( int i )
+	{
+		print( "Beat " + i );
+	}
+
 	// Use this for initialization
 	void Start () {
 		
