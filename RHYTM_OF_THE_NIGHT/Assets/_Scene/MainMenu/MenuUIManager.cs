f3069802befdcafd8c4af6af21e748c36bc22314
dbga_38 <dbga_38@dbga.local>
2018-02-06 17:31:51 +0100

    MainMenu

---
+++ RHYTM_OF_THE_NIGHT/Assets/_Scene/MainMenu/MenuUIManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUIManager : MonoBehaviour 
+{
+	public GameObject mainMenu;
+	public GameObject volumeSlider;
+	public GameObject videoPlayer;
+	public GameObject exitGameChoice; 
+	void Start () 
+	{
+		mainMenu.SetActive (true);
+		videoPlayer.SetActive (false);
+		volumeSlider.SetActive (false);
+		exitGameChoice.SetActive (false);
+	}
+
+	public void MainMenu () 
+	{
+		mainMenu.SetActive (true);
+		videoPlayer.SetActive (false);
+		volumeSlider.SetActive (false);
+		exitGameChoice.SetActive (false);
+	}
+
+	public void StartGame ()
+	{
+		SceneManager.LoadScene ("Setup");
+	}
+
+	public void ExitGame ()
+	{
+		Application.Quit();
+	}
+
+	public void VolumeOption ()
+	{
+		mainMenu.SetActive (false);
+		videoPlayer.SetActive (false);
+		volumeSlider.SetActive (true);
+		exitGameChoice.SetActive (false);
+	}
+
+	public void Tutorial ()
+	{
+		mainMenu.SetActive (false);
+		videoPlayer.SetActive (true);
+		volumeSlider.SetActive (false);
+		exitGameChoice.SetActive (false);
+	}
+
+	public void ExitGameChoice ()
+	{
+		mainMenu.SetActive (false);
+		videoPlayer.SetActive (false);
+		volumeSlider.SetActive (false);
+		exitGameChoice.SetActive (true);
+	}
+}
---
+++ RHYTM_OF_THE_NIGHT/Assets/_Scenes/Menu/Timer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+	public float time;
+
+	public void WaitFor(){
+		
+	}
+
+	public void LoadScene(){
+		
+	}
+
+}
