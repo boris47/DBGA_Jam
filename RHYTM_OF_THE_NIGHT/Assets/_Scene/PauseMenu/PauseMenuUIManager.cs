912b46980a39bce32120f75f1d50c99f00f3a3a9
dbga_38 <dbga_38@dbga.local>
2018-02-06 19:05:51 +0100

    PauseMenu Prototype

---
+++ RHYTM_OF_THE_NIGHT/Assets/_Scene/PauseMenu/PauseMenuUIManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuUIManager : MonoBehaviour {
+
+	public GameObject pauseMenu;
+	public GameObject choice;
+	public GameObject pausePanel;
+
+	void Start () 
+	{
+		pausePanel.SetActive (true);
+		pauseMenu.SetActive (true);
+		choice.SetActive (false);
+	}
+
+	public void BackToGame ()
+	{
+		pausePanel.SetActive (false);
+	}
+
+	public void PauseMenuExitGameChoice ()
+	{
+		choice.SetActive (true);
+		pauseMenu.SetActive (false);
+	}
+
+	public void PauseMenuExitGameChoiceNo ()
+	{
+		pauseMenu.SetActive (true);
+		choice.SetActive (false);
+	}
+
+	public void PauseMenuExitGame ()
+	{
+		Application.Quit();
+	}
+}
