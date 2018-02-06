691afb3c398653ad8a3706d66c1dcdec0446e175
Stefano Mauri <dbga_42@dbga.local>
2018-02-06 17:25:08 +0100

    Aggiornamento della mia scena, Spot.cs creato. Ora si può capire se si sta premendo un bottone sinistro oppure destro

---
+++ RHYTM_OF_THE_NIGHT/Assets/_Scripts/Spot.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Spot : MonoBehaviour, IPointerClickHandler {
+
+	#region Public 
+
+	public bool big;
+
+	#endregion 
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		if (eventData.button == PointerEventData.InputButton.Left) 
+		{
+			
+			Debug.Log ("Left click");
+
+			if (gameObject.tag == "blue") {
+
+				Debug.Log ("Corretto");
+
+			} 
+			else 
+			{
+
+				Debug.Log ("Errato");
+
+			}
+
+		} 
+		else if (eventData.button == PointerEventData.InputButton.Middle) 
+		{
+			Debug.Log ("Middle click");
+		}
+		else if (eventData.button == PointerEventData.InputButton.Right) 
+		{
+			Debug.Log ("Right click");
+
+			if (gameObject.tag == "red") {
+
+				Debug.Log ("Corretto");
+
+			} 
+			else 
+			{
+
+				Debug.Log ("Errato");
+
+			}
+		}
+	}
+}
