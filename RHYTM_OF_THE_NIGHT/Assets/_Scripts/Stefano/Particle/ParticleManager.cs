713a4ef434b1d4b0e9a46bc828c4491b753f1328
Stefano Mauri <dbga_42@dbga.local>
2018-02-07 18:13:31 +0100

    Aggiunti particle system

---
+++ RHYTM_OF_THE_NIGHT/Assets/_Scripts/Stefano/Particle/ParticleManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour 
+{
+
+	[Header("Particle system da spawnare")]
+	public GameObject Hero;
+	public GameObject Ok;
+	public GameObject Late;
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if (Input.GetKeyDown (KeyCode.A)) {
+
+			Hero.SetActive (true);
+
+		}
+
+		if (Input.GetKeyDown (KeyCode.S)) {
+
+			Ok.SetActive (true);
+
+		}
+
+		if (Input.GetKeyDown (KeyCode.D)) {
+
+			Late.SetActive (true);
+
+		}
+
+	}
+}
---
+++ RHYTM_OF_THE_NIGHT/Assets/_Scripts/Stefano/Particle/ParticleTiming.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleTiming : MonoBehaviour {
+
+	public float lifeTime;
+
+	private float timer = 0;
+
+	void Update()
+	{
+
+		timer += Time.deltaTime;
+
+		if (timer >= lifeTime) 
+		{
+
+			timer = 0;
+			gameObject.SetActive (false);
+
+		}
+
+	}
+
+}
