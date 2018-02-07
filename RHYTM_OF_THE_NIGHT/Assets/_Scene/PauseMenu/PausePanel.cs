6b5be0ced2ac81eb0f202f5c890e87ef6d49af6d
DEVOTI-SURFACE\fabio <fabio.devoti@gmail.com>
2018-02-07 12:04:24 +0100

    Pause Menu implemented script -> da testare

---
+++ RHYTM_OF_THE_NIGHT/Assets/_Scene/PauseMenu/PausePanel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PausePanel : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenu;
+    public GameObject pauseButton;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown(buttonName:"pauseButton"))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume ()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause ()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
