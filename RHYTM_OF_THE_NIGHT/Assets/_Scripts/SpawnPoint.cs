60ddea04c896fa2d736377d256c6764612214ab0
Vincenzo Villani <v.villani84@alice.it>
2018-02-07 11:41:33 +0100

    Push dello spawn point per poter cominciare a creare i livelli e di un prefab generico da cui poter creare altri con paramentri specifici e animazione di movimento della UI ( non animazione della sprite, ma di spostamente( pattern ) )

---
+++ RHYTM_OF_THE_NIGHT/Assets/_Scripts/SpawnPoint.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour {
+
+    public GameObject EnemyToSpawn;
+
+    public GameObject Enemy;
+
+    private  void    Start()
+    {
+        if (EnemyToSpawn == null)
+            return;
+
+        Enemy =  Instantiate(EnemyToSpawn, transform.position, Quaternion.identity, transform.parent);
+        Enemy.SetActive( false );
+    }
+
+
+    public  void    Show()
+    {
+        Enemy.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        Enemy.SetActive(false);
+    }
+
+
+
+
+}
