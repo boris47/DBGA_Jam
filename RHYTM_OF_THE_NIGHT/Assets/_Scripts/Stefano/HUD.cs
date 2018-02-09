e096edb41be336dd55fc91d5f3380016da15663f
Vincenzo Villani <v.villani84@alice.it>
2018-02-09 17:11:32 +0100

    Update player and hud

--- RHYTM_OF_THE_NIGHT/Assets/_Scripts/Stefano/HUD.cs
+++ RHYTM_OF_THE_NIGHT/Assets/_Scripts/Stefano/HUD.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class HUD : MonoBehaviour 
@@ -8,36 +6,16 @@ public class HUD : MonoBehaviour
 
 	public Image barHP;
 	public Image barScore;
-	[Range(0f,0.1f)]
-	public float moltiplicatoreScore = 1;
-	[Range(0f,1f)]
-	public float value = 1;
-	public int score = 0;
-
-	public bool isTesting = true;
-
-	void Update() 
-	{
-
-		if (isTesting == true) 
-		{
-			isTesting = false;
-
-			AddScore (1);
-
-		}
-
-	}
 
 	#region Score
 
 	/// <summary>
 	/// Metodo che aggiunge score
 	/// </summary>
-	public void AddScore(int value)
+	public void UpdateScoreBar(int value)
 	{
 
-		barScore.fillAmount += value * moltiplicatoreScore;
+        barScore.fillAmount += value;
 
 	}
 
@@ -48,20 +26,10 @@ public class HUD : MonoBehaviour
 	/// <summary>
 	/// Metodo che toglie valore alla barra della vita
 	/// </summary>
-	public void ReduceBar()
+	public void UpdateLifeBar()
 	{
 
 		barHP.fillAmount -= 0.1f;
-
-	}
-
-	/// <summary>
-	/// Metodo che aggiunge valore alla barra della vita
-	/// </summary>
-	public void IncreaseBar()
-	{
-
-		barHP.fillAmount += 0.1f;
 
 	}
 
